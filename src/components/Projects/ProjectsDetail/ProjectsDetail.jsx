a2cffd2dccf80cec6599382e92fcdcefab74c284
Almond Tuazon <azirtuazon@gmail.com>
2025-03-24 21:44:39 +0800

    ProjectsDetail-page-component

---
+++ src/components/Projects/ProjectsDetail/ProjectsDetail.jsx
@@ -0,0 +1,35 @@
+import Techstack from "@/components/Techstack/Techstack";
+import React from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "ACTDO Mangement Information System",
+    techStack: ["html", "css", "js", "MySql"],
+    githubLink: "/",
+    projectSummary: "explain ko muna",
+    sampleImageProject: ["none", "none", "none", "none"],
+    aim: ["none", "none", "none"],
+    technologiesUsed: [
+      {
+        frontEnd: "asasdasd",
+        backEnd: "asdasdasd",
+        dataBase: "asdasdasd",
+      },
+    ],
+    dataBaseDiagram: "image",
+    implementation: [
+      {
+        frontEnd: ["asasdasd", "asasdasd", "asasdasd"],
+        backEnd: ["asdasdasd", "asasdasd", "asasdasd"],
+        dataBase: ["asdasdasd", "asasdasd", "asasdasd"],
+      },
+    ],
+  },
+];
+
+const ProjectsDetail = () => {
+  return <div>ProjectsDetail</div>;
+};
+
+export default ProjectsDetail;
